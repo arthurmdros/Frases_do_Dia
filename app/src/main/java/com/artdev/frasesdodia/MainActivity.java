c96c953e4e248e609eb16e61532cfcd39a9a148f
arthurmdros <arthurmedeiros2005@gmail.com>
2022-03-01 10:38:10 -0300

    Adicionando método de gerar nova frase

--- app/src/main/java/com/artdev/frasesdodia/MainActivity.java
+++ app/src/main/java/com/artdev/frasesdodia/MainActivity.java
@@ -3,6 +3,10 @@ package com.artdev.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newPhrase(View v){
+        String[] phrases = {
+                "Seja seu principal incentivador!",
+                "Persista não desista!",
+                "Quando o ser faz planos Deus dá risadas",
+                "Ta esperando o que?"
+        };
+        int number =  new Random().nextInt(4);
+
+        TextView phrase = findViewById(R.id.newPhrase);
+        phrase.setText(phrases[number]);
+
     }
 }
